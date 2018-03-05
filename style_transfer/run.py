4ea107601cc5b8104ff5575b6a9d74d6542fd0e4
Lily Xu <lily.l.xu.18@dartmouth.edu>
2018-03-05 07:40:58 -0500

    Setup for style transfer

--- style_transfer/run.py
+++ style_transfer/run.py
@@ -2,21 +2,27 @@ import os
 import numpy as np
 import skimage
 import skimage.io
-from style_transfer.transfer import Transfer
+from transfer import Transfer
 
 WIDTH = 244
 HEIGHT = 244
 
-DATA_INPUT = "style_transfer/data/input/"
-DATA_OUTPUT = "style_transfer/data/output/"
+DATA_INPUT = "data/input/"
+DATA_OUTPUT = "data/output/"
 
-style_path = os.path.join(DATA_INPUT,"style/vangogh.jpg")
-content_path = os.path.join(DATA_INPUT,"content/baker.jpg")
+style_path = os.path.join(DATA_INPUT, "style/vangogh.jpg")
+content_path = os.path.join(DATA_INPUT, "content/baker.jpg")
 
 synthetic_name = os.path.splitext(style_path)[0].split("/")[-1]
 synthetic_name += "_on_"
 synthetic_name += os.path.splitext(content_path)[0].split("/")[-1]
 
 transfer = Transfer(style_path, content_path, WIDTH, HEIGHT)
-transfer.transfer_only_content(img = True, step_size = 0.001, iters = 100, out_dir = DATA_OUTPUT)
+
+# test content transfer
+# transfer.transfer_only_content(img = True, step_size = 0.001, iters = 100, out_dir = DATA_OUTPUT)
+
+# test style transfer
+transfer.transfer_only_style(step_size = 10.0, iters = 100, out_dir = DATA_OUTPUT)
+
 
