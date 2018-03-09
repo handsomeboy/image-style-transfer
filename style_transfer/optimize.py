c6fe7d526633ea4311da936a89821da492a93307
bailymiller <bailey.m.miller.18@dartmouth.edu>
2018-03-09 00:38:23 -0500

    updated optimize methods and added extra params

--- style_transfer/optimize.py
+++ style_transfer/optimize.py
@@ -23,11 +23,14 @@ class SGD:
   def optimize(self):
     params = copy.copy(self.params) 
     params['loss'] = []
-    last_update = 0
-    gradient, loss = (0,0)
+    update = 0
 
+    grad_hist = 0
+    step_hist = 0
+
+    gradient, loss = (0,0)
     params['init_display'](params)
-    for i in range(params['iters']):
+    for i in range(1,params['iters']):
       if params['type'] == 'sgd':
         grad, loss = params['dJdTheta'](params['theta'])
         update = params['step_size'] * grad
@@ -38,11 +41,36 @@ class SGD:
 
       elif params['type'] == 'nesterov':
         loss = params['J'](params['theta'])
-        grad, _ = params['dJdTheta'](params['theta'] - params['gamma'] * last_update)
-        update = params['gamma'] * last_update + params['step_size'] * grad
+        grad, _ = params['dJdTheta'](params['theta'] - params['gamma'] * update)
+        update = params['gamma'] * update + params['step_size'] * grad
 
-      params['theta'] -= update
-      last_update = update
+      elif params['type'] == 'adagrad':
+        grad, loss = params['dJdTheta'](params['theta'] - params['beta'] * update) 
+        grad_hist += np.square(grad)
+        update = params['step_size'] * np.divide(grad, 1e-6 + np.sqrt(grad_hist))
+
+      elif params['type'] == 'adadelta':
+        # http://climin.readthedocs.io/en/latest/adadelta.html
+        grad, loss = params['dJdTheta'](params['theta'] - params['beta'] * update)
+  
+        # Accumulate gradient.
+        grad_hist = np.add(np.multiply(params['gamma'], grad_hist), 
+                           np.multiply((1.0 - params['gamma']), np.square(grad)))
+        
+        # delta_theta = sqrt(step_hist / grad_hist) * grad
+        delta_theta = params['step_size'] * np.multiply(np.divide(np.sqrt(step_hist + params['eps']),
+                                                                  np.sqrt(grad_hist + params['eps'])),
+                                                        grad)
+
+        # Accumulate steps
+        step_hist = np.add(np.multiply(params['gamma'], step_hist), 
+                           np.multiply((1.0 - params['gamma']), np.square(delta_theta)))
+        
+        update = delta_theta
+
+      sigma_noise = np.sqrt(params['step_size'] / (1 + i) ** params['gamma_normal'])
+      noise = np.zeros(update.shape) if params['add_noise'] else np.random.uniform(0,sigma_noise,update.shape)
+      params['theta'] -= update + noise
       params['loss'].append(loss)
       params['iter'] = i
       
--- style_transfer/run.py
+++ style_transfer/run.py
@@ -20,11 +20,20 @@ synthetic_name += os.path.splitext(content_path)[0].split("/")[-1]
 
 transfer = Transfer(style_path, content_path, WIDTH, HEIGHT,
                     initial = None,
-                    content_layers = ["conv1_2", "conv4_2"])
+                    content_layers = ["conv4_2"])
 
 # test content transfer
-#transfer.transfer_only_content(step_size = 1000, iters = 100, out_dir = DATA_OUTPUT)
+transfer.transfer_only_content(out_dir = DATA_OUTPUT, params = {
+                                'type' : 'nesterov',
+                                'step_size' : 100,
+                                'iters' : 30,
+                                'gamma' : 0.9,
+                                'gamma_normal' : 20,
+                                'add_noise' : False,
+                                'eps' : 1,
+                                'beta' : 0.0,
+                              })
 
 # test style transfer
-transfer.transfer_only_style(step_size = 1.0e-9, iters = 100, out_dir = DATA_OUTPUT)
+#transfer.transfer_only_style(step_size = 1.0e-9, iters = 100, out_dir = DATA_OUTPUT)
 
